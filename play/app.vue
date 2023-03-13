f9419305874cf760833c9a2358d6d880afa2d5c7
lilian97 <13642147806@163.com>
2023-03-13 23:59:00 +0800

    init

---
+++ play/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ea-button type="primary">主要按钮</ea-button>
+    <!-- <Button>主要按钮</Button> -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+// import { Button } from "easyest";
+</script>
